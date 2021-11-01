ac00e85b5b66efe7d0ee07899da977ac3c73a79d
Aditya Aggarwal <85849078+adityaaggarwal12@users.noreply.github.com>
2021-11-01 17:29:33 +0530

    Create Question_2.cpp

---
+++ task_2/Question_2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#define ll long long
+
+using namespace std;
+
+void solve(int t){
+    while (t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+        int k;
+        cin>>k;
+        sort(arr,arr+n);
+        cout << k << " th element is " << arr[k-1] << "\n";
+        for(int i=2;i<=arr[k-1]/2;i++){
+            if(arr[k-1]%i==0){
+                cout  << k<< " th element is not prime";
+                return ;
+            }
+        }
+        cout  << k<< " th element is prime";
+
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    solve(t);
+    return 0;
+}
